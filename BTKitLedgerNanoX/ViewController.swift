fc2c1453a1ec6cf0225a0193f135f37f87a23b5e
Rinat Enikeev <rinat.enikeev@gmail.com>
2022-05-01 15:02:40 +0300

    Initial Commit

---
+++ BTKitLedgerNanoX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTKitLedgerNanoX
+//
+//  Created by Rinat Enikeev on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
